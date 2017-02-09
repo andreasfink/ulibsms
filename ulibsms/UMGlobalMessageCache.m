16ac57a5cf376414eba56c9a13123a7668f424c7
Andreas Fink <andreas@fink.org>
2017-02-09 08:40:22 +0100

    cleanup of copyright lines

--- ulibsms/UMGlobalMessageCache.m
+++ ulibsms/UMGlobalMessageCache.m
@@ -2,21 +2,11 @@
 //  UMGlobalMessageCache.m
 //  ulibsms
 //
-//  © 2016  by Andreas Fink
+//  Copyright © 2017 Andreas Fink (andreas@fink.org). All rights reserved.
 //
 // This source is dual licensed either under the GNU GENERAL PUBLIC LICENSE
 // Version 3 from 29 June 2007 and other commercial licenses available by
 // the author.
-//
-
-#import "UMGlobalMessageCache.h"
-
-//
-//  UMGlobalMessageCache.m
-//  MessageMover
-//
-//  Created by Andreas Fink on 06/07/15.
-//
 //
 
 #import "UMGlobalMessageCache.h"
