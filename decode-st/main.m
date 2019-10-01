086a57ce973c1e24c64f76f8808c64f509c275b1
Andreas Fink <andreas@fink.org>
2019-10-01 11:51:40 +0200

    decode-st

--- decode-st/main.m
+++ decode-st/main.m
@@ -13,10 +13,13 @@ void DecodeST(NSData *data);
 
 int main(int argc, const char * argv[])
 {
-    NSString *s = @"027000002e0d00000000505348000000000000421e811c200201618516702d112181020d0cf468656c6c6f20776f726c642b00";
+#if 0
+   // NSString *s = @"027000002e0d00000000505348000000000000421e811c200201618516702d112181020d0cf468656c6c6f20776f726c642b00";
+    NSString *s=@"00501115001500b00010da16d560989b32c302bcf9e3a68d55663d58d794468020a6dd8692037a2d9719b39e489220de600645a6409ca954247542ba6b84756d0a864008d4e76ddd97299dc69b3f59d13c18";
+
     NSData *d = [s unhexedData];
     DecodeST(d);
-/*
+#else
     for(int idx=1;idx<argc;idx++)
     {
         @autoreleasepool
@@ -26,7 +29,7 @@ int main(int argc, const char * argv[])
             DecodeST(d);
         }
     }
- */
+#endif
     return 0;
 }
 
@@ -35,11 +38,12 @@ void DecodeST(NSData *data)
     uint8_t *bytes = (uint8_t *) data.bytes;
     NSInteger len = data.length;
     NSInteger p = 0;
-    
+    /*
     for(int i=0;i<len;i++)
     {
-        NSLog(@"Byte[%d] = %02X =  %d",i,bytes[i],bytes[i]);
+        fprintf(stdout,"Byte[%d] = %02X =  %d\n",i,bytes[i],bytes[i]);
     }
+     */
     /* 3GPP TS 23.048 version 5.9.0 Release 5 17 ETSI TS 123 048 V5.9.0 (2005-06)*/
     
     /* UDH Header */
@@ -51,7 +55,7 @@ void DecodeST(NSData *data)
         ((bytes[1]==0x70) || (bytes[1]==0x71)) &&
         (bytes[3]==0x00))
     {
-        NSLog(@"Skipping UDH header 02 70 00");
+        fprintf(stdout,"Skipping UDH header 02 70 00\n");
         p += 3;
     }
     int CPL = 0;
@@ -68,7 +72,7 @@ void DecodeST(NSData *data)
     if((p+1)<len)
     {
         CPL = (bytes[p] << 8) | bytes[p+1];
-        NSLog(@"CPL: %d (%02X %02X)",CPL,bytes[p],bytes[p+1]);
+        fprintf(stdout,"CPL:      %d (%02X %02X)\n",CPL,bytes[p],bytes[p+1]);
         p = p + 2;
     }
 
@@ -88,7 +92,7 @@ void DecodeST(NSData *data)
     TAR_bytes[0] = bytes[p];
     TAR_bytes[1] = bytes[p+1];
     TAR_bytes[2] = bytes[p+2];
-    TAR = (bytes[p] << 16) || (bytes[p+1] << 8) || bytes[p+2];
+    TAR = (TAR_bytes[0] << 16) | (TAR_bytes[1] << 8) | TAR_bytes[2];
     p+=3;
     CNTR[0] = bytes[p++];
     CNTR[1] = bytes[p++];
@@ -105,74 +109,74 @@ void DecodeST(NSData *data)
     p = p + j;
 
     
-    NSLog(@"CHL: %d (%02X)",CHL,CHL);
-    NSLog(@"SPI: %d (%02X %02X)",SPI, ((SPI>>8) & 0xFF), (SPI & 0xFF));
+    fprintf(stdout,"CHL:      0x%02X (%d)\n",CHL,CHL);
+    fprintf(stdout,"SPI:      0x%02X%02X (%d)\n",((SPI>>8) & 0xFF), (SPI & 0xFF),SPI);
 
     int c;
-    NSLog(@"    X X X - - - - -   . . . . . . . . Reserved");
+    fprintf(stdout,"          X X X - - - - -   . . . . . . . . Reserved\n");
     c = (SPI >> 11) & 0x03;
     switch(c)
     {
         case 0:
-            NSLog(@"    - - - 0 0 - - -   . . . . . . . . No counter available");
+            fprintf(stdout,"          - - - 0 0 - - -   . . . . . . . . No counter available\n");
             break;
         case 1:
-            NSLog(@"    - - - 0 1 - - -   . . . . . . . . Counter available; no replay or sequence checking");
+            fprintf(stdout,"          - - - 0 1 - - -   . . . . . . . . Counter available; no replay or sequence checking\n");
             break;
         case 2:
-            NSLog(@"    - - - 1 0 - - -   . . . . . . . . Process if and only if counter value is higher than the value in the RE");
+            fprintf(stdout,"          - - - 1 0 - - -   . . . . . . . . Process if and only if counter value is higher than the value in the RE\n");
             break;
         case 3:
-            NSLog(@"    - - - 1 1 - - -   . . . . . . . . Process if and only if counter value is one higher than the value in the RE");
+            fprintf(stdout,"          - - - 1 1 - - -   . . . . . . . . Process if and only if counter value is one higher than the value in the RE\n");
             break;
     }
     c = (SPI >> 10) & 0x01;
     switch(c)
     {
         case 0:
-            NSLog(@"    - - - - - 0 - -   . . . . . . . . No Ciphering");
+            fprintf(stdout,"          - - - - - 0 - -   . . . . . . . . No Ciphering\n");
             break;
         case 1:
-            NSLog(@"    - - - - - 1 - -   . . . . . . . . Ciphering");
+            fprintf(stdout,"          - - - - - 1 - -   . . . . . . . . Ciphering\n");
             break;
     }
     c = (SPI >> 8) & 0x03;
     switch(c)
     {
         case 0:
-            NSLog(@"    - - - - - - 0 0   . . . . . . . . No RC, CC or DS");
+            fprintf(stdout,"          - - - - - - 0 0   . . . . . . . . No RC, CC or DS\n");
             break;
         case 1:
-            NSLog(@"    - - - - - - 0 1   . . . . . . . . Redundancy Check");
+            fprintf(stdout,"          - - - - - - 0 1   . . . . . . . . Redundancy Check\n");
             break;
         case 2:
-            NSLog(@"    - - - - - - 1 0   . . . . . . . . Cryptographic Checksum");
+            fprintf(stdout,"          - - - - - - 1 0   . . . . . . . . Cryptographic Checksum\n");
             break;
         case 3:
-            NSLog(@"    - - - - - - 1 1   . . . . . . . . Digital Signature");
+            fprintf(stdout,"          - - - - - - 1 1   . . . . . . . . Digital Signature\n");
             break;
     }
 
-    NSLog(@"    . . . . . . . .   X X - - - - - - Reserved");
+    fprintf(stdout,"          . . . . . . . .   X X - - - - - - Reserved\n");
 
     c = (SPI >> 5) & 0x01;
     switch(c)
     {
         case 0:
-            NSLog(@"    . . . . . . . .   - - 0 - - - - - PoR response shall be sent using SMS-DELIVER-REPORT");
+            fprintf(stdout,"          . . . . . . . .   - - 0 - - - - - PoR response shall be sent using SMS-DELIVER-REPORT\n");
             break;
         case 1:
-            NSLog(@"    . . . . . . . .   - - 1 - - - - - PoR response shall be sent using SMS-SUBMIT");
+            fprintf(stdout,"          . . . . . . . .   - - 1 - - - - - PoR response shall be sent using SMS-SUBMIT\n");
             break;
     }
     c = (SPI >> 4) & 0x01;
     switch(c)
     {
         case 0:
-            NSLog(@"    . . . . . . . .   - - - 0 - - - - PoR response shall not be ciphered");
+            fprintf(stdout,"          . . . . . . . .   - - - 0 - - - - PoR response shall not be ciphered\n");
             break;
         case 1:
-            NSLog(@"    . . . . . . . .   - - - 1 - - - - PoR response shall be ciphered");
+            fprintf(stdout,"          . . . . . . . .   - - - 1 - - - - PoR response shall be ciphered\n");
             break;
     }
     c = (SPI >> 2) & 0x03;
@@ -180,125 +184,127 @@ void DecodeST(NSData *data)
     switch(c)
     {
         case 0:
-            NSLog(@"    . . . . . . . .   - - - - 0 0 - - No RC, CC or DS applied to PoR response to SE");
+            fprintf(stdout,"          . . . . . . . .   - - - - 0 0 - - No RC, CC or DS applied to PoR response to SE\n");
             break;
         case 1:
-            NSLog(@"    . . . . . . . .   - - - - 0 1 - - PoR response with simple RC applied to it");
+            fprintf(stdout,"          . . . . . . . .   - - - - 0 1 - - PoR response with simple RC applied to it\n");
             break;
         case 2:
-            NSLog(@"    . . . . . . . .   - - - - 1 0 - - PoR response with CC applied to it");
+            fprintf(stdout,"          . . . . . . . .   - - - - 1 0 - - PoR response with CC applied to it\n");
             break;
         case 3:
-            NSLog(@"    . . . . . . . .   - - - - 1 1 - - PoR response with DS applied to it");
+            fprintf(stdout,"          . . . . . . . .   - - - - 1 1 - - PoR response with DS applied to it\n");
             break;
     }
     c = (SPI >> 0) & 0x03;
     switch(c)
     {
         case 0:
-            NSLog(@"    . . . . . . . .   - - - - - - 0 0 No PoR reply to the Sending Entity (SE)");
+            fprintf(stdout,"          . . . . . . . .   - - - - - - 0 0 No PoR reply to the Sending Entity (SE)\n");
             break;
         case 1:
-            NSLog(@"    . . . . . . . .   - - - - - - 0 1 PoR required to be sent to the SE");
+            fprintf(stdout,"          . . . . . . . .   - - - - - - 0 1 PoR required to be sent to the SE\n");
             break;
         case 2:
-            NSLog(@"    . . . . . . . .   - - - - - - 1 0 PoR required only when an error has occured");
+            fprintf(stdout,"          . . . . . . . .   - - - - - - 1 0 PoR required only when an error has occured\n");
             break;
         case 3:
-            NSLog(@"    . . . . . . . .   - - - - - - 1 1 Reserved");
+            fprintf(stdout,"          . . . . . . . .   - - - - - - 1 1 Reserved\n");
             break;
     }
 
-    NSLog(@"KIc: %d",KIc);
+    fprintf(stdout,"KIc:      0x%02X (%d)\n",KIc,KIc);
 
-    NSLog(@"    %d %d %d %d - - - -                   indication of Keys to be used", ((KIc >>7)?1:0),((KIc >>6)?1:0),((KIc >>5)?1:0),((KIc >>4)?1:0) );
+    fprintf(stdout,"          %d %d %d %d - - - -                   indication of Keys to be used\n", ((KIc >>7)?1:0),((KIc >>6)?1:0),((KIc >>5)?1:0),((KIc >>4)?1:0) );
 
     c = (KIc >> 2) & 0x03;
     switch(c)
     {
         case 0:
-            NSLog(@"    - - - - 0 0 - -                   DES in CBC mode");
+            fprintf(stdout,"          - - - - 0 0 - -                   DES in CBC mode\n");
             break;
         case 1:
-            NSLog(@"    - - - - 0 1 - -                   Triple DES in outer-CBC mode using two different keys");
+            fprintf(stdout,"          - - - - 0 1 - -                   Triple DES in outer-CBC mode using two different keys\n");
             break;
         case 2:
-            NSLog(@"    - - - - 1 0 - -                   Triple DES in outer-CBC mode using three different keys");
+            fprintf(stdout,"          - - - - 1 0 - -                   Triple DES in outer-CBC mode using three different keys\n");
             break;
         case 3:
-            NSLog(@"    - - - - 1 1 - -                   DES in ECB mode");
+            fprintf(stdout,"          - - - - 1 1 - -                   DES in ECB mode\n");
             break;
     }
     c = (KIc >> 0) & 0x03;
     switch(c)
     {
         case 0:
-            NSLog(@"    - - - - - - 0 0                   Algorithm known implicitly by both entities");
+            fprintf(stdout,"          - - - - - - 0 0                   Algorithm known implicitly by both entities\n");
             break;
         case 1:
-            NSLog(@"    - - - - - - 0 1                   DES");
+            fprintf(stdout,"          - - - - - - 0 1                   DES\n");
             break;
         case 2:
-            NSLog(@"    - - - - - - 1 0                   Reserved");
+            fprintf(stdout,"          - - - - - - 1 0                   Reserved\n");
             break;
         case 3:
-            NSLog(@"    - - - - - - 1 1                   proprietary Implementations");
+            fprintf(stdout,"          - - - - - - 1 1                   proprietary Implementations\n");
             break;
     }
 
-    NSLog(@"KID: %d",KID);
-    NSLog(@"    %d %d %d %d - - - -                   indication of Keys to be used", ((KID >>7)?1:0),((KID >>6)?1:0),((KID >>5)?1:0),((KID >>4)?1:0) );
+    fprintf(stdout,"KID:      0x%02X (%d)\n",KID,KID);
+    fprintf(stdout,"          %d %d %d %d - - - -                   indication of Keys to be used\n", ((KID >>7)?1:0),((KID >>6)?1:0),((KID >>5)?1:0),((KID >>4)?1:0) );
 
     c = (KID >> 2) & 0x03;
     switch(c)
     {
         case 0:
-            NSLog(@"    - - - - 0 0 - -                   DES in CBC mode");
+            fprintf(stdout,"          - - - - 0 0 - -                   DES in CBC mode\n");
             break;
         case 1:
-            NSLog(@"    - - - - 0 1 - -                   Triple DES in outer-CBC mode using two different keys");
+            fprintf(stdout,"          - - - - 0 1 - -                   Triple DES in outer-CBC mode using two different keys\n");
             break;
         case 2:
-            NSLog(@"    - - - - 1 0 - -                   Triple DES in outer-CBC mode using three different keys");
+            fprintf(stdout,"          - - - - 1 0 - -                   Triple DES in outer-CBC mode using three different keys\n");
             break;
         case 3:
-            NSLog(@"    - - - - 1 1 - -                   DES in ECB mode");
+            fprintf(stdout,"          - - - - 1 1 - -                   DES in ECB mode\n");
             break;
     }
     c = (KID >> 0) & 0x03;
     switch(c)
     {
         case 0:
-            NSLog(@"    - - - - - - 0 0                   Algorithm known implicitly by both entities");
+            fprintf(stdout,"          - - - - - - 0 0                   Algorithm known implicitly by both entities\n");
             break;
         case 1:
-            NSLog(@"    - - - - - - 0 1                   DES");
+            fprintf(stdout,"          - - - - - - 0 1                   DES\n");
             break;
         case 2:
-            NSLog(@"    - - - - - - 1 0                   Reserved");
+            fprintf(stdout,"          - - - - - - 1 0                   Reserved\n");
             break;
         case 3:
-            NSLog(@"    - - - - - - 1 1                   proprietary Implementations");
+            fprintf(stdout,"          - - - - - - 1 1                   proprietary Implementations\n");
             break;
     }
 
-    NSLog(@"TAR: %02X %02X %02X (%d)",TAR_bytes[0],TAR_bytes[1],TAR_bytes[2],TAR);
-    NSLog(@"CNTR: %02X %02X %02X %02X %02X",CNTR[0],CNTR[1],CNTR[2],CNTR[3],CNTR[4]);
-    NSLog(@"PCNTR: %d",PCNTR);
+    fprintf(stdout,"TAR:      0x%02X%02X%02X (%d)\n",TAR_bytes[0],TAR_bytes[1],TAR_bytes[2],TAR);
+    fprintf(stdout,"CNTR:     0x%02X%02X%02X%02X%02X\n",CNTR[0],CNTR[1],CNTR[2],CNTR[3],CNTR[4]);
+    fprintf(stdout,"PCNTR:    0x%2X (%d)\n",PCNTR,PCNTR);
     NSMutableString *s = [[NSMutableString alloc]init];
     for(int i=0;i<j;i++)
     {
-        [s appendFormat:@" %02X",RC_CC_DS[i]];
+        [s appendFormat:@"%02X",RC_CC_DS[i]];
     }
 
-    NSLog(@"RC_CC_DS:%@",s);
+    fprintf(stdout,"RC_CC_DS: 0x%s\n",s.UTF8String);
 
     s = [[NSMutableString alloc]init];
-    for(int i=p;i<len;i++)
+    for(NSInteger i=p;i<len;i++)
     {
         [s appendFormat:@" %02X",bytes[i]];
+        if((i-p+1)%16==0)
+        {
+            [s appendString:@"\n         "];
+        }
     }
-    NSLog(@"Payload:%@",s);
-
-
+    fprintf(stdout,"Payload: %s\n",s.UTF8String);
 }
