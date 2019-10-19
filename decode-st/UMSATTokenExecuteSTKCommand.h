8e21b1203b22c7e63a01df7e1b49673409643064
Andreas Fink <andreas@fink.org>
2019-10-19 06:41:51 +0200

    work in progress

--- decode-st/UMSATTokenExecuteSTKCommand.h
+++ decode-st/UMSATTokenExecuteSTKCommand.h
@@ -8,10 +8,74 @@
 
 #import "UMSATToken.h"
 
+typedef enum UMSAT_STKCommand_Type
+{
+    STK_REFRESH                 = 0x01,
+    STK_MORE_TIME               = 0x02,
+    STK_POLL_INTERVAL           = 0x03,
+    STK_POLLING_OFF             = 0x04,
+    STK_SET_UP_EVENT_LIST       = 0x05,
+    STK_SET_UP_CALL             = 0x10,
+    STK_GSM_3G_SEND_SS          = 0x11,
+    STK_GSM_3G_SEND_USSD        = 0x12,
+    STK_SEND_SHORT_MESSAGE      = 0x13,
+    STK_SEND_DTMF               = 0x14,
+    STK_LAUNCH_BROWSER                      = 0x15,
+    STK_3GPP_GEOGRAPHICAL_LOCATION_REQUEST  = 0x16,
+    STK_PLAY_TONE                           = 0x20,
+    STK_DISPLAY_TEXT                        = 0x21,
+    STK_GET_INKEY                           = 0x22,
+    STK_GET_INPUT                           = 0x23,
+    STK_SELECT_ITEM                         = 0x24,
+    STK_SET_UP_MENU                         = 0x25,
+    STK_PROVIDE_LOCAL_INFORMATION           = 0x26,
+    STK_TIMER_MANAGEMENT                    = 0x27,
+    STK_SET_UP_IDLE_MODE_TEXT               = 0x28,
+    STK_PERFORM_CARD_APDU                   = 0x30,
+    STK_POWER_ON_CARD_                      = 0x31,
+    STK_POWER_OFF_CARD                      = 0x32,
+    STK_GET_READER_STATUS                   = 0x33,
+    STK_RUN_AT_COMMAND                      = 0x34,
+    STK_LANGUAGE_NOTIFICATION               = 0x35,
+    STK_OPEN_CHANNEL                        = 0x40,
+    STK_CLOSE_CHANNEL                       = 0x41,
+    STK_RECEIVE_DATA                        = 0x42,
+    STK_SEND_DATA                           = 0x43,
+    STK_GET_CHANNEL_STATUS                  = 0x44,
+    STK_SERVICE_SEARCH                      = 0x45,
+    STK_GET_SERVICE_INFORMATION             = 0x46,
+    STK_DECLARE_SERVICE                     = 0x47,
+    STK_SET_FRAMES                          = 0x50,
+    STK_GET_FRAMES_STATUS                   = 0x51,
+    STK_RETRIEVE_MULTIMEDIA_MESSAGE         = 0x60,
+    STK_SUBMIT_MULTIMEDIA_MESSAGE           = 0x61,
+    STK_DISPLAY_MULTIMEDIA_MESSAGE          = 0x62,
+    STK_ACTIVATE                            = 0x70,
+    STK_CONTACTLESS_STATE_CHANGED           = 0x71,
+    STK_COMMAND_CONTAINER                   = 0x72,
+    STK_ENCAPSULATED_SESSION_CONTROL        = 0x73,
+    STK_eUICC_OPERATION                     = 0x74,
+ //   STK_End_of_the_proactive_UICC_session_not_applicable    = 0x81,
+    STK_RESERVED_PROPRIETARY_USE_F0         = 0xF0,
+    STK_RESERVED_PROPRIETARY_USE_F1         = 0xF1,
+    STK_RESERVED_PROPRIETARY_USE_F2         = 0xF2,
+    STK_RESERVED_PROPRIETARY_USE_F3         = 0xF3,
+    STK_RESERVED_PROPRIETARY_USE_F4         = 0xF4,
+    STK_RESERVED_PROPRIETARY_USE_F5         = 0xF5,
+    STK_RESERVED_PROPRIETARY_USE_F6         = 0xF6,
+    STK_RESERVED_PROPRIETARY_USE_F7         = 0xF7,
+    STK_RESERVED_PROPRIETARY_USE_F8         = 0xF8,
+    STK_RESERVED_PROPRIETARY_USE_F9         = 0xF9,
+    STK_RESERVED_PROPRIETARY_USE_FA         = 0xFA,
+    STK_RESERVED_PROPRIETARY_USE_FB         = 0xFB,
+    STK_RESERVED_PROPRIETARY_USE_FC         = 0xFC,
+    STK_RESERVED_PROPRIETARY_USE_FD         = 0xFD,
+    STK_RESERVED_PROPRIETARY_USE_FE         = 0xFE,
+} UMSAT_STKCommand_Type;
 
 @interface UMSATTokenExecuteSTKCommand : UMSATToken
 {
-    int _command_type;
+    UMSAT_STKCommand_Type _command_type;
     int _command_qualifier;
     int _destination_device;
     NSString *_varId;
--- decode-st/UMSATTokenExecuteSTKCommand.m
+++ decode-st/UMSATTokenExecuteSTKCommand.m
@@ -7,6 +7,7 @@
 //
 
 #import "UMSATTokenExecuteSTKCommand.h"
+static const char *stk_command_name(UMSAT_STKCommand_Type command);
 
 @implementation UMSATTokenExecuteSTKCommand
 
@@ -37,11 +38,176 @@
 {
     NSMutableString *s = [[NSMutableString alloc]init];
 
-    [s appendFormat:@"%@Command-Type:       0x%02X\n",ident,_command_type];
+    [s appendFormat:@"%@Command-Type:       0x%02X (%s)\n",ident,(int)_command_type, stk_command_name(_command_type)];
     [s appendFormat:@"%@Command-Qualifier:  0x%02X\n",ident,_command_qualifier];
-    [s appendFormat:@"%@Destination-Device: 0x%02X\n",ident,_destination_device];
+    [s appendFormat:@"%@Destination-Device: 0x%02X",ident,_destination_device];
+    switch(_destination_device)
+    {
+        case 0x01:
+            [s appendFormat:@" (Keypad)"];
+            break;
+        case 0x02:
+            [s appendFormat:@" (Display)"];
+            break;
+        case 0x81:
+            [s appendFormat:@" (UICC)"];
+            break;
+        case 0x82:
+            [s appendFormat:@" (Terminal)"];
+            break;
+        case 0x83:
+            [s appendFormat:@" (Network)"];
+            break;
+        default:
+            if((_destination_device >= 0x10) & (_destination_device <=0x17))
+            {
+                [s appendFormat:@" (Additional Card Reader x (0 to 7))"];
+            }
+            else if((_destination_device >= 0x20) & (_destination_device <=0x27))
+            {
+                [s appendFormat:@" (Channel with Channel identifier x (1 to 7))"];
+            }
+            else if((_destination_device >= 0x30) & (_destination_device <=0x3F))
+            {
+                [s appendFormat:@" (eCAT client identifier (1 to F))"];
+            }
+            else
+            {
+                [s appendFormat:@" (reserved value)"];
+            }
+    }
+    [s appendFormat:@"\n"];
 
     return s;
 }
 
 @end
+
+const char *stk_command_name(UMSAT_STKCommand_Type command)
+{
+    switch(command)
+    {
+    case STK_REFRESH:
+        return "REFRESH";
+    case STK_MORE_TIME:
+        return "MORE_TIME";
+    case STK_POLL_INTERVAL:
+        return "POLL_INTERVAL";
+    case STK_POLLING_OFF:
+        return "POLLING_OFF";
+    case STK_SET_UP_EVENT_LIST:
+        return "SET_UP_EVENT_LIST";
+    case STK_SET_UP_CALL:
+        return "SET_UP_CALL";
+    case STK_GSM_3G_SEND_SS:
+        return "GSM_3G_SEND_SS";
+    case STK_GSM_3G_SEND_USSD:
+        return "GSM_3G_SEND_USSD";
+    case STK_SEND_SHORT_MESSAGE:
+        return "SEND_SHORT_MESSAGE";
+    case STK_SEND_DTMF:
+        return "SEND_DTMF";
+    case STK_LAUNCH_BROWSER:
+        return "LAUNCH_BROWSER";
+    case STK_3GPP_GEOGRAPHICAL_LOCATION_REQUEST:
+        return "3GPP_GEOGRAPHICAL_LOCATION_REQUEST";
+    case STK_PLAY_TONE:
+        return "PLAY_TONE";
+    case STK_DISPLAY_TEXT:
+        return "DISPLAY_TEXT";
+    case STK_GET_INKEY:
+        return "GET_INKEY";
+    case STK_GET_INPUT:
+        return "GET_INPUT";
+    case STK_SELECT_ITEM:
+        return "SELECT_ITEM";
+    case STK_SET_UP_MENU:
+        return "SET_UP_MENU";
+    case STK_PROVIDE_LOCAL_INFORMATION:
+        return "PROVIDE_LOCAL_INFORMATION";
+    case STK_TIMER_MANAGEMENT:
+        return "TIMER_MANAGEMENT";
+    case STK_SET_UP_IDLE_MODE_TEXT:
+        return "SET_UP_IDLE_MODE_TEXT";
+    case STK_PERFORM_CARD_APDU:
+        return "PERFORM_CARD_APDU";
+    case STK_POWER_ON_CARD_:
+        return "POWER_ON_CARD_";
+    case STK_POWER_OFF_CARD:
+        return "POWER_OFF_CARD";
+    case STK_GET_READER_STATUS:
+        return "GET_READER_STATUS";
+    case STK_RUN_AT_COMMAND:
+        return "RUN_AT_COMMAND";
+    case STK_LANGUAGE_NOTIFICATION:
+        return "LANGUAGE_NOTIFICATION";
+    case STK_OPEN_CHANNEL:
+        return "OPEN_CHANNEL";
+    case STK_CLOSE_CHANNEL:
+        return "CLOSE_CHANNEL";
+    case STK_RECEIVE_DATA:
+        return "RECEIVE_DATA";
+    case STK_SEND_DATA:
+        return "SEND_DATA";
+    case STK_GET_CHANNEL_STATUS:
+        return "GET_CHANNEL_STATUS";
+    case STK_SERVICE_SEARCH:
+        return "SERVICE_SEARCH";
+    case STK_GET_SERVICE_INFORMATION:
+        return "GET_SERVICE_INFORMATION";
+    case STK_DECLARE_SERVICE:
+        return "DECLARE_SERVICE";
+    case STK_SET_FRAMES:
+        return "SET_FRAMES";
+    case STK_GET_FRAMES_STATUS:
+        return "GET_FRAMES_STATUS";
+    case STK_RETRIEVE_MULTIMEDIA_MESSAGE:
+        return "RETRIEVE_MULTIMEDIA_MESSAGE";
+    case STK_SUBMIT_MULTIMEDIA_MESSAGE:
+        return "SUBMIT_MULTIMEDIA_MESSAGE";
+    case STK_DISPLAY_MULTIMEDIA_MESSAGE:
+        return "DISPLAY_MULTIMEDIA_MESSAGE";
+    case STK_ACTIVATE:
+        return "ACTIVATE";
+    case STK_CONTACTLESS_STATE_CHANGED:
+        return "CONTACTLESS_STATE_CHANGED";
+    case STK_COMMAND_CONTAINER:
+        return "COMMAND_CONTAINER";
+    case STK_ENCAPSULATED_SESSION_CONTROL:
+        return "ENCAPSULATED_SESSION_CONTROL";
+    case STK_eUICC_OPERATION:
+        return "eUICC_OPERATION";
+    case STK_RESERVED_PROPRIETARY_USE_F0:
+        return "RESERVED_PROPRIETARY_USE_F0";
+    case STK_RESERVED_PROPRIETARY_USE_F1:
+        return "RESERVED_PROPRIETARY_USE_F1";
+    case STK_RESERVED_PROPRIETARY_USE_F2:
+        return "RESERVED_PROPRIETARY_USE_F2";
+    case STK_RESERVED_PROPRIETARY_USE_F3:
+        return "RESERVED_PROPRIETARY_USE_F3";
+    case STK_RESERVED_PROPRIETARY_USE_F4:
+        return "RESERVED_PROPRIETARY_USE_F4";
+    case STK_RESERVED_PROPRIETARY_USE_F5:
+        return "RESERVED_PROPRIETARY_USE_F5";
+    case STK_RESERVED_PROPRIETARY_USE_F6:
+        return "RESERVED_PROPRIETARY_USE_F6";
+    case STK_RESERVED_PROPRIETARY_USE_F7:
+        return "RESERVED_PROPRIETARY_USE_F7";
+    case STK_RESERVED_PROPRIETARY_USE_F8:
+        return "RESERVED_PROPRIETARY_USE_F8";
+    case STK_RESERVED_PROPRIETARY_USE_F9:
+        return "RESERVED_PROPRIETARY_USE_F9";
+    case STK_RESERVED_PROPRIETARY_USE_FA:
+        return "RESERVED_PROPRIETARY_USE_FA";
+    case STK_RESERVED_PROPRIETARY_USE_FB:
+        return "RESERVED_PROPRIETARY_USE_FB";
+    case STK_RESERVED_PROPRIETARY_USE_FC:
+        return "RESERVED_PROPRIETARY_USE_FC";
+    case STK_RESERVED_PROPRIETARY_USE_FD:
+        return "RESERVED_PROPRIETARY_USE_FD";
+    case STK_RESERVED_PROPRIETARY_USE_FE:
+        return "RESERVED_PROPRIETARY_USE_FE";
+    default:
+        return "Unknown";
+    }
+}
